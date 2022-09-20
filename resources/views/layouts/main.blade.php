9e0d1531d293ebe3a3592b6a42611147bb2fc2bb
buracha <jadsoncanuto6@gmail.com>
2022-09-20 10:44:58 -0300

    Criando layout com blade

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>@yield('title')</title>
+
+        <!-- Fonte do Google -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans" rel="stylesheet">
+
+        <!-- CSS Bootstrap -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+        <!-- CSS Aplicação -->
+            <link rel="stylesheet" href="/css/styles.css">
+            <script src="/js/script.js"></script>
+
+    </head>
+    <body>
+        @yield('content')
+        <footer>
+            <p>HDC Events &copy; 2020</p>
+        </footer>
+    </body>
+</html>
--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,1 +1,9 @@
+@extends('layouts.main')
+
+@section('tilte', 'Produtos')
+
+@section('content')
+
 <h1>Pagina de Produtos</h1>
+
+@endsection
